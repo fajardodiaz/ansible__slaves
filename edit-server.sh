d7eb866958f7e5b3dfd817ddae26d0cf05afa79a
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-12-15 16:02:10 -0500

    test new config

--- edit-server.sh
+++ edit-server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo sed -i "s/#server.rmi.localport=4000/server.rmi.localport=4000/" /opt/apache-jmeter-5.5/bin/jmeter.properties
+
+sudo echo "server.rmi.localport=4000" /opt/apache-jmeter-5.5/bin/jmeter.properties
 sudo echo "java.rmi.server.hostname=$(hostname -I | awk '{print $1}')" >> /opt/apache-jmeter-5.5/bin/jmeter.properties
