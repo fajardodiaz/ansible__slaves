f637757e62116678653fbf2c581f5485628e42bd
Pablo Fajardo <pfajardodiaz17@gmail.com>
2022-12-19 08:46:01 -0500

    Adding apt update

--- edit-server.sh
+++ edit-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo echo "server.rmi.localport=4000" /opt/apache-jmeter-5.5/bin/jmeter.properties
-sudo echo "java.rmi.server.hostname=$(hostname -I | awk '{print $1}')" >> /opt/apache-jmeter-5.5/bin/jmeter.properties
-sudo echo "jmeterengine.nongui.port=4445" >> /opt/apache-jmeter-5.5/bin/jmeter.properties
+# sudo echo "server.rmi.localport=4000" /opt/apache-jmeter-5.5/bin/jmeter.properties
+# sudo echo "java.rmi.server.hostname=$(hostname -I | awk '{print $1}')" >> /opt/apache-jmeter-5.5/bin/jmeter.properties
+# sudo echo "jmeterengine.nongui.port=4445" >> /opt/apache-jmeter-5.5/bin/jmeter.properties
